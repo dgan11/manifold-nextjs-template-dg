508a1d2bb5500c342c8b3bff12d2b9def04bb45a
David Gan <dgan979@gmail.com>
2022-11-04 10:02:32 -0700

    fix nextjs fallback provider issues with script tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  return (
+    <>
+      {/* <Script src="https://connect.manifoldxyz.dev/2.0.13/connect.umd.min.js"></Script>
+      <Script src="https://marketplace.manifoldxyz.dev/latest/marketplace.umd.min.js"></Script> */}
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
